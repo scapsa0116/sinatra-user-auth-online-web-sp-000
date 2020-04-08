6e90b1a9ca2c3373d14c0697bd6671ea19d72aaf
Stela Capsa <stela.capsa@gmail.com>
2020-04-08 19:30:55 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20200408152920) do
+
+  create_table "user", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+  end
 
 end
